fb76701d7b9dc7971cc05943d8851eb1ae71ade4
rob <rjgallac@hotmail.com>
2022-06-22 21:43:11 +0100

    first

---
+++ README.md
@@ -0,0 +1,5 @@
+
+use x64 command prompt( this comes as part of visual studio code build tools)
+
+mvnw -Pnative -DskipTests package
+
---
+++ src/main/java/com/example/nativedemo/NativedemoApplication.java
@@ -0,0 +1,38 @@
+package com.example.nativedemo;
+
+import com.steinf.pafaddressformat.DeliveryPoint;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.Banner;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+
+@SpringBootApplication
+public class NativedemoApplication implements CommandLineRunner {
+
+	@Value("${postCode}")
+	private String postCode;
+
+	public static void main(String[] args) {
+
+//		SpringApplication.run(NativedemoApplication.class, args);
+		new SpringApplicationBuilder(NativedemoApplication.class)
+				.logStartupInfo(false)
+				.bannerMode(Banner.Mode.OFF)
+				.run(args);
+	}
+	@Override
+	public void run(String... args) throws Exception {
+		DeliveryPoint deliveryPoint = new DeliveryPoint.Builder()
+				.withSubBuildingName("A")
+				.withBuildingNumber("12")
+				.withThroughfare("HIGH STREET NORTH")
+				.withDependentLocality("COOMBE BISSETT")
+				.withPostTown("SALISBURY")
+				.withPostcode(postCode)
+
+				.build();
+		System.out.println(deliveryPoint.toString());
+	}
+}
